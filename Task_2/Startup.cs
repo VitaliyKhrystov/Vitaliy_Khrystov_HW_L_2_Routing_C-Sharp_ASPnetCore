5bc26dcc9b548ca5a3f13016aff39faa466d1cec
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-23 12:34:07 +0300

    Created Task 2

---
+++ Task_2/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Task_2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // AddMvc - добавляет сервисы, необходимые для работы MVC, включая Razor-страницы
+            // services.AddMvc();
+
+            // AddControllersWithViews - добавляет сервисы, необходимые для работы MVC, 
+            // не включая Razor-страницы - только контроллеры и представления.
+            // Для примеров данного курса этого достаточно.
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "Default",
+                        pattern: "{controller=Home}/{action=Index}/{*data}"
+                    );
+            });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //            name: "ProductsList",
+            //            pattern: "{controller=Products}/{action=List}/{data?}"
+            //        );
+            //});
+
+        }
+    }
+}
